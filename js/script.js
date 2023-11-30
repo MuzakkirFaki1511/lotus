18fd5ab8eaf60e6306b80ea3c968c5e5e0c372cb
MuzakkirFaki1511 <checkmuzakkir@gmail.com>
2023-11-30 14:48:37 +0530

    Expected changes

--- js/script.js
+++ js/script.js
@@ -10,3 +10,23 @@ window.onscroll = () => {
   menu.classList.remove("fa-times");
   navbar.classList.remove("active");
 };
+
+// for booking through whatsapp
+function sendWhatsAppMessage() {
+  // Fetch details from the form
+  const name = document.getElementById("name").value;
+  const number = document.getElementById("number").value;
+  const email = document.getElementById("email").value;
+  const date = document.getElementById("date").value;
+
+  // Construct the WhatsApp message with appointment request line
+  const whatsappMessage = `Appointment booking request for:\nName: ${name}\nNumber: ${number}\nEmail: ${email}\nDate: ${date}`;
+
+  // Create the WhatsApp link using wa.me with pre-filled message
+  const whatsappLink = `https://wa.me/+917378618618?text=${encodeURIComponent(
+    whatsappMessage
+  )}`;
+
+  // Open the link in a new tab
+  window.open(whatsappLink, "_blank");
+}
